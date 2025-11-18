3f1991b8930c8898a8192170eb0524dbefe993d3
Ananya807 <69763771+Ananya807@users.noreply.github.com>
2025-11-18 16:46:55 -0700

    Update Code.m

--- EV3 Matlab Code/Code.m
+++ EV3 Matlab Code/Code.m
@@ -1,75 +1,104 @@
-
-%0	No color (Unknown color) 
-%1	Black 
-    %2	Blue 
-    %6	Green 
-    %4	Yellow 
-    %5	Red 
-%3	White 
-%7	Brown
-
-% Set desired distance from the wall
-closeDistance = 15;   % cm (too close)
-farDistance = 30;   % cm (too far)
-distance1 = brick.UltrasonicDist(1);
-distance2 = brick.UltrasonicDist(3);
+% Title: EV3 Robot Sensor-Guided Navigation Loop
+global key
 color = brick.ColorCode(4);
 while true
-    brick.MoveMotor('BC', 25);
-    if distance1 < closeDistance
-        % If too close to the wall, steer left
-        brick.MoveMotor('BC', 0);
+    distanceRight = brick.UltrasonicDist(1);  % Get distance to the right wall
+    distanceForward = brick.UltrasonicDist(3);  % Get distance forward
+    display("Distance Forward: " + distanceForward);
+    display("Distance to the Right: " + distanceRight);
+    pause(0.1);
+    brick.MoveMotor('BC', 20);
+    color = brick.ColorCode(4);
+    display(color);
+    if distanceForward < 25
+        brick.MoveMotor('BC', -35);
+        pause(1);
+        brick.MoveMotor('B', -25);
+        brick.MoveMotor('C', 26);
+        pause(1);
+        brick.StopMotor('BC');
+    elseif distanceRight > 40
+        brick.MoveMotor('C', -20);
+        brick.MoveMotor('B', 20);
         pause(0.5);
-        brick.MoveMotor('B', 5);
-        brick.MoveMotor('C', 25);
-        pause(1);
-        brick.MoveMotor('C', 5);
-        brick.MoveMotor('B', 25);
-        pause(1);
-        distance1 = brick.UltrasonicDist(1);
-    elseif distance1 > farDistance && distance1 < 55
-        % If too far from the wall, steer right
-        brick.MoveMotor('BC', 0);
-        pause(0.5);
-        brick.MoveMotor('C', 5);
-        brick.MoveMotor('B', 25);
-        pause(1);
-        brick.MoveMotor('B', 5);
-        brick.MoveMotor('C', 25);
-        pause(1);
-        distance1 = brick.UltrasonicDist(1);
-    elseif distance1 > 55
-        while distance1 > 55
-        brick.MoveMotor('BC', 0);
-        pause(1);
-        brick.MoveMotor('B', 50);
-        brick.MoveMotor('C', 20)
-        pause(2);
-        brick.MoveMotor('BC', 25)
+        brick.MoveMotor('BC', 20);
         pause(2.5);
-        end
-    else
-        % If at the desired distance, continue moving forward
-        brick.MoveMotor('BC', 25); % Move forward
-        distance1 = brick.UltrasonicDist(1); % Update distance
-        pause(0.5); % Short pause to allow for distance measurement
-    end
-
-    display(distance2);
-    distance2 = brick.UltrasonicDist(3);
-    if distance2 < 25 && distance2 > 15
-        brick.MoveMotor('BC', 0);
-        pause(1)
-        display(distance2);
-        brick.MoveMotor('B', -10); % Rotate
-        brick.MoveMotor('C', 10); % Rotate
-        pause(3.3);
-        brick.MoveMotor('BC', 0); % Stop rotation
-        pause(1);
-    end
-    if distance2 < 15
-        brick.MoveMotor('BC', -15);
+        brick.StopMotor('BC');
+    elseif distanceRight < 15
+        brick.MoveMotor('C', 20);
+        brick.MoveMotor('B', -20);
+        pause(0.5);
+        brick.MoveMotor('BC', 20);
         pause(2);
+        brick.StopMotor('BC');
     end
-       
+    if color == 5           % red
+        brick.StopMotor('BC', 0);
+        pause(1);
+        brick.MoveMotor('BC', 20);
+    elseif color == 6 || color == 2   % Green or Blue
+        InitKeyboard();
+        while true
+        brick.StopMotor('BC', 'brake');
+            pause(0.1);
+            switch key
+                case 'uparrow'
+                    disp('Up Arrow Pressed!');
+                    brick.MoveMotor('BC', 50);
+                    pause(0.5);
+                    brick.MoveMotor('BC', 0);
+                    pause(0.25);
+                case 'downarrow'
+                    disp('Down Arrow Pressed!');
+                    brick.MoveMotor('BC', -20);
+                    pause(0.75);
+                    brick.MoveMotor('BC', 0);
+                    pause(0.25);
+                case 'leftarrow'
+                    disp('Left Arrow Pressed!');
+                    brick.MoveMotor('B', -10);
+                    brick.MoveMotor('C', 10);
+                    pause(0.75);
+                    brick.MoveMotor('BC', 0);
+                    pause(0.25);
+                case 'rightarrow'
+                    disp('Right Arrow Pressed!');
+                    brick.MoveMotor('B', 10);
+                    brick.MoveMotor('C', -10);
+                    pause(0.75);
+                    brick.MoveMotor('BC', 0);
+                    pause(0.25);
+                case '1'
+                    brick.MoveMotor('A', 30);
+                    pause(0.5);
+                    brick.MoveMotor('A', 0);
+                    pause(0.25);
+                case '2'
+                    brick.MoveMotor('A', -10);
+                    pause(0.25);
+                    brick.MoveMotor('A', 0);
+                    pause(0.25);
+                case '3'
+                    brick.MoveMotor('A', 999);
+                    pause(0.2);
+                    brick.MoveMotor('A', -100);
+                    pause(0.2);
+                    brick.MoveMotor('A', 999);
+                    pause(0.2);
+                    brick.MoveMotor('A', -100);
+                    pause(0.2);
+                    brick.MoveMotor('A', 999);
+                    pause(0.2);
+                    brick.MoveMotor('A', -100);
+                    pause(0.2);
+                    brick.MoveMotor('ABC', 0);                    
+                case 0 % No key is being pressed.
+                    disp('No Key Pressed!');
+                case 'q' % Press "q" to quit.
+                    break;
+            end
+            brick.MoveMotor('BC', 0);
+        end
+        CloseKeyboard();
+    end
 end
